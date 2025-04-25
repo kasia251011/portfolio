4a40c10c7b45874c184b90e72adf0a223bdba72b
Kasia Jankowska <katarzyna.jankowska000728@gmail.com>
2025-04-25 12:37:23 +0200

    Fix exp card

--- src/components/ExpCard/index.tsx
+++ src/components/ExpCard/index.tsx
@@ -8,7 +8,7 @@ interface ExpCardProps {
 }
 
 const ExpCard = ({ title, description, icon }: ExpCardProps) => (
-  <div className="bg-white rounded-4xl flex flex-col items-center p-10 pb-16">
+  <div className="bg-white rounded-4xl flex flex-col items-center p-10 pb-16 h-full">
     <Image src={icon} alt={title} className="w-10 h-10 mb-2" />
     <h3 className="my-2">{title}</h3>
     <p className="text-sm text-gray-500">{description}</p>
